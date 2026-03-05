f4519fe39b211b6e50cbd6b91c3989d369978440
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-03-05 12:45:26 +0100

    style(estado_jugador): :art: Formatear codigo usando Ruff

--- src/estado_jugador.py
+++ src/estado_jugador.py
@@ -1,33 +1,39 @@
-class EstadoJugador():
-    
+class EstadoJugador:
     def registrar_gol(self, jugadora):
         pass
-    
+
     def registrar_asisencia(self, jugadora):
         pass
-    
+
     def __str__(self):
         return self.__class__.__name__
-    
-    
+
+
 class JugadorActivo(EstadoJugador):
-    
-    def registrar_gol(self,jugadora):
+    def registrar_gol(self, jugadora):
         jugadora.goles += 1
-    
+
     def registrar_asisencia(self, jugadora):
         jugadora.asistencias += 1
-        
+
     def __str__(self):
         return "[ACTIVA]"
-    
+
+
 class JugadorSancionado(EstadoJugador):
-    
     def registrar_gol(self, jugadora):
-        print("[AVISO] " + jugadora.nombre + " está en la penalty box y no puede marcar goles.")
-    
+        print(
+            "[AVISO] "
+            + jugadora.nombre
+            + " está en la penalty box y no puede marcar goles."
+        )
+
     def registrar_asisencia(self, jugadora):
-        print("[AVISO] " + jugadora.nombre + " está en la penalty box y no puede dar asistencias.")
-        
+        print(
+            "[AVISO] "
+            + jugadora.nombre
+            + " está en la penalty box y no puede dar asistencias."
+        )
+
     def __str__(self):
         return "[SANCIONADA]"
