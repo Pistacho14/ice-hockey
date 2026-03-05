de91fd0e93a270d5a0c201af6446c5ef9dc9470f
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-03-05 12:34:09 +0100

    feat(estado_jugador): :sparkles: Añadir clase abstracta

---
+++ src/estado_jugador.py
@@ -0,0 +1,30 @@
+class EstadoJugador():
+    
+    def registrar_gol(self, jugadora):
+        pass
+    
+    def registrar_asisencia(self, jugadora):
+        pass
+    
+    
+class JugadorActivo(EstadoJugador):
+    
+    def registrar_gol(self,jugadora):
+        jugadora.goles += 1
+    
+    def registrar_asisencia(self, jugadora):
+        jugadora.asistenias += 1
+        
+    def __str__(self):
+        return "ACTIVA"
+    
+class JugadorSancionado(EstadoJugador):
+    
+    def registrar_gol(self, jugadora):
+        print("[AVISO] " + jugadora.nombre + " está en la penalty box y no puede marcar goles.")
+    
+    def registrar_asisencia(self, jugadora):
+        print("[AVISO] " + jugadora.nombre + " está en la penalty box y no puede dar asistencias.")
+        
+    def __str__(self):
+        return "SANCIONADA"
